683e34817ff7494d64b9aa359e91851232d2c682
lusolegonzalez <lusolegonzalez@gmail.com>
2025-07-16 10:19:55 -0300

    set firebase keys

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,15 @@ import cors from 'cors';
 import 'dotenv/config';
 import admin from 'firebase-admin';
 import fs from 'fs';
- const serviceKey = JSON.parse(fs.readFileSync('./firebase-key.json','utf8')); 
- admin.initializeApp({ credential: admin.credential.cert(serviceKey) });
+const serviceKey = JSON.parse(fs.readFileSync('./firebase-key.json','utf8')); 
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
+});
 
 const __dirname = path.resolve(); 
 
